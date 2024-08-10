fff0b9548e93c356605da0d1dc865bfca222eeed
unknown <kirthip19shetty@gmail.com>
2024-08-10 13:35:33 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <div className="pt-24">
+       <h1>Footer</h1>
+       <div className="font-medium italic">jsjsjsj</div>
+      </div>
+    );
+  }
+  
+  export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <div className="text-white p-2">
+       <h1>Natural disater</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
